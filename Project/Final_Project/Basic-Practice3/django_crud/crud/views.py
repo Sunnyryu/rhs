81ee2d429a756d74c280475acbf3732fa4150f57
sunnyryu <sunny_ryu@outlook.com>
2019-11-15 12:57:14 +0900

    Django Study & Practice

--- Project/Final_Project/Basic-Practice3/django_crud/crud/views.py
+++ Project/Final_Project/Basic-Practice3/django_crud/crud/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .models import Article
+
 
 # Create your views here.
 def index(request):
     return render(request, 'crud/index.html')
+def new(request):
+    return render(request, 'crud/new.html')
+def create(request):
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+    print(title)
+    print(content)
+    article = Article()
+    article.title = title
+    article.content = content
+    article.save()
+
+    return render(request, 'crud/create.html')
