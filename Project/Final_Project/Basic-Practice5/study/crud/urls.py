fcf55280e4cfb2a2319d2a2a7b3ed6a0b70f1e56
sunnyryu <sunny_ryu@outlook.com>
2019-11-19 08:23:57 +0900

    file auth change & django study & django study

---
+++ Project/Final_Project/Basic-Practice5/study/crud/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = "crud"
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('new/', views.new, name="new"),
+    # path('create/', views.create, name="create"),
+    path('<int:id>', views.detail, name="detail"),
+    path('<int:id>/update/', views.update, name="update"),
+    # path('<int:id>/save/', views.arti_save, name="save"),
+    path('<int:id>/delete/', views.delete, name="delete"),
+]
