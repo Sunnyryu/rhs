d5c02575e9b1c47f49b6a896cfd4109f74d1d709
sunnyryu <sunny_ryu@outlook.com>
2019-11-25 19:31:40 +0900

    test prep

---
+++ GCP/GCP_Practice.md
@@ -0,0 +1,12 @@
+## GCP Basic Study
+
+#### 1. Basic
+
+```
+Google Cloud Platform Console / Cloud shell & Cloud SDK / Rest-based API / Cloud mobile App
+
+등으로 사용됨 !
+
+
+
+```
--- Project/Final_Project/Pre-Test/test1.py
+++ Project/Final_Project/Pre-Test/test1.py
@@ -6,7 +6,7 @@ import pandas as pd
 import csv 
 import openpyxl
 
-pytrends = TrendReq(hl='ko', tz=540)
+pytrends = TrendReq(hl='us', tz=360)
 #hl은 header language => 사용언어라고 이해하자,  en-US / ko-KR 등 
 #tz => timezone => 미국 360 / korea 540 ! 
 # proxies => https proxies Google passed ONLY list ['https://34.203.233.13:80','https://35.201.123.31:880', ..., ...] => 굳이 잘안씀
@@ -16,8 +16,8 @@ pytrends = TrendReq(hl='ko', tz=540)
 
 # list 형식으로 확인하고 싶은 것 넣기 !
 
-mylist = ["김치", "라면", "밥", "김", "쌀"]
-pytrends.build_payload(mylist, cat=0, timeframe='today 5-y', geo='KR', gprop='') 
+mylist = ["hyundai motor", "toyota"]
+pytrends.build_payload(mylist, cat=0, timeframe='today 5-y', geo='US', gprop='') 
 # mylist 는 위의 값 , cat = 카테고리 범주 (Category to narrow results) / timeframe => 일자 지정 (google trends 참조)  - 'today 5-y' => 5년 전부터 지금까지, all => 모두, 'YYYY-MM-DD YYYY-MM-DD' / 
 #  'YYYY-MM-DDTHH YYYY-MM-DDTHH'(2017-02-06T10 2017-02-12T07)  (UTC를 기초로 함!) 
 #  geo = 데이터를 수집할 코드(국가), gprop = 어느 사이트에서 가져올 지 나타내는 것, 
@@ -27,7 +27,6 @@ pytrends.build_payload(mylist, cat=0, timeframe='today 5-y', geo='KR', gprop='')
 getgaindata = pytrends.interest_over_time()
 del getgaindata['isPartial']
 getgaindata.to_csv('list.csv', encoding="utf-8") # csv 형식으로 파일을 만들어줌 ! / 인코딩은 utf-8로 해줌
-
 ### 추가적인 활용법 
 
 
