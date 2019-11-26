bd3ee91b0b18db9d26ccc34db0a5b62982a1281a
sunnyryu <sunny_ryu@outlook.com>
2019-11-26 11:47:16 +0900

    django study

--- Project/Final_Project/Basic/Basic-Practice10/accounts/views.py
+++ Project/Final_Project/Basic/Basic-Practice10/accounts/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from IPython import embed
 # Create your views here.
 def signup(request):
+    
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        embed()
+        if form.is_valid():
+            form.save()
+            #user = form.save()
+            return redirect('boards:index')
+    else:
+        form = UserCreationForm()
+        #embed()
 
-    form = UserCreationForm()
+    
     context = { 'form':form}
     return render(request, 'accounts/signup.html', context)
