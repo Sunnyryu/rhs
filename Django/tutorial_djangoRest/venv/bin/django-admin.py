2c7193c30a4f7e4ee2bf7d52deae7635a50307b1
sunnyryu <sunny_ryu@outlook.com>
2020-01-30 20:16:43 +0900

    DJango restful api

---
+++ Django/tutorial_djangoRest/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/sunny/ubuntu/Django/tutorial_djangoRest/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ Django/tutorial_serialization/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/sunny/ubuntu/Django/tutorial_serialization/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
