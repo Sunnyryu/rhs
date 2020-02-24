28b01d2a76ca6f7c01f6e65ae9417bbd0201f126
sunnyryu <sunny_ryu@outlook.com>
2020-02-24 18:45:11 +0900

    electron study

---
+++ Electron/electron_prep_1/main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require("electron");
+
+let win;
+
+function createdWindow() {
+  win = new BrowserWindow({ width: 800, height: 600 });
+  win.loadURL('file://$(__dirname}/index.html');
+  win.on("closed", () => { win = null });
+}
+
+app.on("ready", createdWindow);
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit():
+  }
+});
+
+app.on("activate", () => {
+  if (win === null){
+    createdWindow():
+  }
+});
+
